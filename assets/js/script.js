cb27efec76c4e5941e6abff913dc499b55dc223e
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-01-12 18:14:53 +0000

    Added weather data paths to get a City name, temperature, wind, jumidity, icon code in comments for future use

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 // Elements
 var searchInput = $("#search-input");
 var searchBtn = $("#search-button");
+var forcastToday = $("#today");
 
 // var queryURL = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
 var queryURL =
-  "https://api.openweathermap.org/data/2.5/forecast?lat=33.44&&lon=-94.04&appid=3c4f418d697258b26a8f47e2024d5b99";
+  "https://api.openweathermap.org/data/2.5/forecast?lat=51.5072&&lon=0.1276&appid=3c4f418d697258b26a8f47e2024d5b99";
 var queryUrlCityCordinates =
   "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=3c4f418d697258b26a8f47e2024d5b99";
 
@@ -14,8 +15,17 @@ https: fetch(queryURL)
   })
   .then((data) => {
     console.log(data);
+    searchBtn.on("click", (event) => {
+      event.preventDefault();
+      // Display city name - console.log(data.city.name);
+      // Temp console.log((data.list[0].main.temp - 273.15).toFixed(2));
+      // Wind console.log(data.list[0].wind.speed);
+      // Humidity console.log(data.list[0].main.humidity);
+      // Icon code console.log(data.list[0].weather[0].icon);
+      // Icon url: http://openweathermap.org/img/w/10d.png
+      var forcastTodayCity = $(
+        `<img src="http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png">`
+      );
+      forcastToday.append(forcastTodayCity);
+    });
   });
-
-searchBtn.on("click", function () {
-  var
-});
