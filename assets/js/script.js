235939fd77812d9954ca05961a50563c4ad643d2
Ginjak <89803801+Ginjak@users.noreply.github.com>
2024-01-12 17:45:56 +0000

    Added generic queryUrl from open weather map for City latitude/longtitue and queryUrl for weather details

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,20 @@
 var searchInput = $("#search-input");
 var searchBtn = $("#search-button");
 
-console.log(searchInput);
-console.log(searchBtn);
+// var queryURL = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
+var queryURL =
+  "https://api.openweathermap.org/data/2.5/forecast?lat=33.44&&lon=-94.04&appid=3c4f418d697258b26a8f47e2024d5b99";
+var queryUrlCityCordinates =
+  "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=3c4f418d697258b26a8f47e2024d5b99";
+
+https: fetch(queryURL)
+  .then((resp) => {
+    return resp.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
+
+searchBtn.on("click", function () {
+  var
+});
